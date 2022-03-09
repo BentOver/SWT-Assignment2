16ae0cde43e3ba2b50551d274d5ae6cc0850c5c6
Jonathan Damsgaard Hald <au673052@uni.au.dk>
2022-03-09 15:56:31 +0100

    Inital commit

--- SWT-Assignment2/Program.cs
+++ SWT-Assignment2/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+				// Assemble your system here from all the classes
+
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        door.OnDoorOpen();
+                        break;
+
+                    case 'C':
+                        door.OnDoorClose();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.OnRfidRead(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
+        }
+    }
+}
