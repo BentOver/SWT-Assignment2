6f810f8e2a9c973ecc329254fcbeb525e6aea527
Wespa S <jswespa@gmail.com>
2022-03-10 14:04:24 +0100

    Created IChargeControl

---
+++ LadeSkabClassLibrary/Interfaces/IChargeControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeSkabClassLibrary.Controls
+{
+    internal class IChargeControl
+    {
+        event EventHandler<ChargeControlEventArgs> ChargeControlEvent;
+    }
+}
